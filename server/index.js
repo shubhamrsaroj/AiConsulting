83ab905a59a80cfa4ce8d848e75ae0557e79eccb
Shubham Rajesh Saroj <57361979+shubhamrsaroj@users.noreply.github.com>
2026-01-31 18:11:36 +0530

    Added link in index.js

--- server/index.js
+++ server/index.js
@@ -10,11 +10,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: "https://aiconsultinglimited.netlify.app/" }));
 app.use(express.json());
 
 // MongoDB Connection
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://shubhamrsaroj229:sinpluscos12@atlascluster.1rzraz5.mongodb.net/ai-consulting?retryWrites=true&w=majority&appName=AtlasCluster';
+const MONGODB_URI = process.env.MONGODB_URI;
 
 mongoose.connect(MONGODB_URI)
   .then(() => console.log('MongoDB Connected'))
